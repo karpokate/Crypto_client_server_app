470723400676686ddf3f9ac2435857772cbffe4d
Katheryna <k.karpo.a@gmail.com>
2020-12-12 01:50:56 +0200

    UPD in settings part

--- back_end_part/back_end_part/settings.py
+++ back_end_part/back_end_part/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4^vct!g@f)z*7=n-yoz*z2q*z@jt)s&r&a)an-#=+%w_$2sp(n'
+from back_end_part.back_end_part.Select import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
